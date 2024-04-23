4cd6ad36b49dc74f67e66c026fc24160989a04de
William A. Sease <WilliamASease@gmail.com>
2024-04-23 03:13:14 -0400

    You're getting a PROMOTION

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { isMobile } from "react-device-detect";
+
+const robotnik_bangers = ['YapeV5XBgQ4','HnyGSl3K-IE'].join(',')
 
 function App() {
   return (
@@ -7,19 +8,24 @@ function App() {
       style={{
         display: "flex",
         flexDirection: "column",
-        backgroundColor: "lightgrey",
         alignItems: "center",
         minHeight: "100%",
         width: "100%",
       }}
     >
-      <div>
-        Your application's content goes here. Mobile users won't be able to
-        see the grey bars on the side.
-      </div>
-      <div style={{position:'fixed', bottom:50, right:10, height:"40%", width:"25%"}}>
+      <iframe 
+      style={{position:'fixed', bottom:50, left:10}}
+      width="50" 
+      height="50"
+      src={`https://www.youtube.com/embed/YapeV5XBgQ4?si=fqZyhoEsYYaoxkLL&autoplay=1&playlist=${robotnik_bangers}`}
+      title="YouTube video player" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      >
+
+       </iframe>
+      <div style={{position:'fixed', bottom:50, right:10, height:"40%", width:"25%", background:'white'}}>
         <div>Let us know how much you HATE that hedgehog</div>
-        <iframe src="https://chitchatter.im/public/robotnik-radio?embed=1" allow="camera;microphone;display-capture;fullscreen" width="100%" height="100%" />
+        <iframe title="chitchatter" src="https://chitchatter.im/public/robotnik-radio?embed=1" allow="camera;microphone;display-capture;fullscreen" width="100%" height="100%" />
       </div>
     </div>
   );
